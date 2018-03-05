45e825b3ab4448b6be74619316b3746be067876d
Aaron <aaron.miller2112@gmail.com>
2018-03-05 05:05:14 -0800

    Function Declarations added
    Added function declarations in CShipInfo.cpp

--- CShipInfo.cpp
+++ CShipInfo.cpp
@@ -4,6 +4,51 @@ namespace SINK_THE_FLEET
 {
 	CShipInfo::CShipInfo()
 	{
-
+	}
+	void CShipInfo::print(ostream & sout) const
+	{
+	}
+	void CShipInfo::print() const
+	{
+	}
+	CShip CShipInfo::getName() const
+	{
+		return CShip();
+	}
+	CDirection CShipInfo::getOrientation() const
+	{
+		return CDirection();
+	}
+	CCell CShipInfo::getBowLocation() const
+	{
+		return CCell();
+	}
+	short CShipInfo::getPiecesLeft() const
+	{
+		return 0;
+	}
+	bool CShipInfo::isSunk()
+	{
+		return false;
+	}
+	void CShipInfo::setName()
+	{
+	}
+	void CShipInfo::setOrientation()
+	{
+	}
+	void CShipInfo::setBowLocation()
+	{
+	}
+	void CShipInfo::setPiecesLeft()
+	{
+	}
+	CShipInfo CShipInfo::operator--()
+	{
+		return CShipInfo();
+	}
+	ostream & operator<<(ostream & sout, const CShipInfo & shipInfo)
+	{
+		
 	}
 }
