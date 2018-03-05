cc5bef9c9bd835bdb82e7440f49361584bcde571
InfinityOmelette <paul.m.jacobson13@gmail.com>
2018-03-04 21:30:24 -0800

    Completed version of the CDirection class (Don't forget to change the Direction2 enum name back to Direction after we delete the fleet.h and fleet.cpp files)

--- CDirection.cpp
+++ CDirection.cpp
@@ -2,8 +2,40 @@
 
 namespace SINK_THE_FLEET
 {
-	CDirection::CDirection()
+
+	CDirection::operator Direction2() const
 	{
-		// test
+		return m_direction;
+	}
+
+	void CDirection::print(ostream &sout)const
+	{
+		if (m_direction)	// if not zero, therefore 1, therefore vertical
+			sout << "V";
+		else
+			sout << "H";
+	}
+
+	void CDirection::print()const
+	{
+		print(cout);
+	}
+
+	ostream &operator<<(ostream &sout, const CDirection &direction)
+	{
+		direction.print(sout);
+		return sout;
+	}
+
+	istream &operator>>(istream &sin, CDirection &direction)
+	{
+		char input;
+		sin >> input;
+		if (input == 'V')
+			direction = CDirection(VERTICAL);
+		if (input == 'H')
+			direction = CDirection(HORIZONTAL);
+		return sin;
+
 	}
 }
