d5b4d9ebd6fcc4779affc18069ad5ef679b57ed4
Albert Shymanskyy <albertshymy@gmail.com>
2018-03-10 22:31:11 -0800

    Small fix

--- CCell.h
+++ CCell.h
@@ -15,16 +15,17 @@ namespace SINK_THE_FLEET
 		void print() const;
 
 		void inputCoordinates(istream &sin, char size);
+
 		// Number of players in game
-const short NUMPLAYERS = 2;
-// Number of rows/columns in small sea array
-const short SMALLROWS = 8;
-// Number of rows/columns in large sea array
-const short LARGEROWS = 10;
-// Number of rows/columns in small sea array
-const short SMALLCOLS = 12;
-// Number of rows/columns in large sea array
-const short LARGECOLS = 24;
+		const short NUMPLAYERS = 2;
+		// Number of rows/columns in small sea array
+		const short SMALLROWS = 8;
+		// Number of rows/columns in large sea array
+		const short LARGEROWS = 10;
+		// Number of rows/columns in small sea array
+		const short SMALLCOLS = 12;
+		// Number of rows/columns in large sea array
+		const short LARGECOLS = 24;
 	private:
 		unsigned short m_row;
 		unsigned short m_col;
